42d20005f2a5c5e2450cc1cdccda09d2054a3611
wow-woody <jjeongwooss1@gmail.com>
2025-10-16 18:51:44 +0900

    2025-10-16(목) 학원 v01 스토어 페이지 취합 -1

---
+++ js/sub_store.js
@@ -0,0 +1,101 @@
+
+let storeItems = document.querySelectorAll(".store");
+let popup = document.querySelector(".popup_wrap");
+let closeBtn = document.querySelector(".popup .close_btn");
+let cityBox = document.querySelector(".city_box");
+let countiesBox = document.querySelector(".counties_box");
+let cityList = document.querySelector(".city_list");
+let countiesList = document.querySelector(".counties_list");
+let mapDetails = document.querySelectorAll(".store .detail");
+
+console.log(storeItems);
+
+mapDetails.forEach((mapDetail) => {
+    mapDetail.addEventListener("mouseenter", () => {
+        mapDetail.classList.add("active");
+    })
+
+    mapDetail.addEventListener("mouseout", () => {
+        mapDetail.classList.remove("active");
+    })
+});
+
+
+//팝업
+storeItems.forEach((storeItem) => {
+    storeItem.addEventListener("click", () => {
+        popup.style.display = "flex";
+
+        setTimeout(() => {
+            map.relayout();
+            map.setCenter(markerPosition);
+        }, 100);
+    });
+})
+
+
+
+closeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    popup.style.display = "none";
+});
+
+//지도
+const mapContainer = document.getElementById('map'),
+    mapOption = {
+        center: new kakao.maps.LatLng(37.5630503, 126.8501181),
+        level: 4
+    };
+
+const map = new kakao.maps.Map(mapContainer, mapOption);
+
+const imageSrc = "../images/bhc-ms.svg",
+    imageSize = new kakao.maps.Size(50, 50),
+    imageOption = { offset: new kakao.maps.Point(27, 69) };
+
+
+const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(37.5618815, 126.8511103); 
+
+const marker = new kakao.maps.Marker({
+    position: markerPosition,
+    image: markerImage 
+});
+
+
+marker.setMap(map);
+map.setCenter(markerPosition);
+
+
+const content = '<div class="customoverlay">' +
+    '  <a href="https://map.kakao.com/link/map/26628353" target="_blank">' +
+    '    <span class="title">BHC가양점</span>' +
+    '  </a>' +
+    '</div>';
+
+// 커스텀 오버레이가 표시될 위치
+const position = new kakao.maps.LatLng(37.5618815, 126.8511103);
+
+// 커스텀 오버레이를 생성
+const customOverlay = new kakao.maps.CustomOverlay({
+    map: map,
+    position: position,
+    content: content,
+    yAnchor: 1
+});
+
+//지역선택
+cityBox.addEventListener("click", () => {
+    cityList.style.display = "block";
+})
+cityList.addEventListener("click", () => {
+    cityList.style.display = "none";
+})
+countiesBox.addEventListener("click", () => {
+    countiesList.style.display = "block";
+})
+countiesList.addEventListener("click", () => {
+    countiesList.style.display = "none";
+})
+
+
